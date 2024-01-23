2c7eca9392a9a1f8bee0eb7ca039d93133a21cba
KiHwan Yun <drg2524@naver.com>
2024-01-23 11:57:18 +0900

    web Config 설정 후 뷰 출력 결과

--- src/main/java/hello/typeconverter/WebConfig.java
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -34,6 +34,22 @@ public class WebConfig implements WebMvcConfigurer {
         *   StringToIntegerConverter, IntegerToStringConverter를 꼭 주석처리 하자.
         *   MyNumberFormatter도 숫자 -> 문자, 문자 -> 숫자로 변경하기 때문에 둘의 기능이 겹친다.
         *   우선순위는 컨버터가 우선하므로 포멧터가 적용되지 않고, 컨버터가 적용된다.
+        *
+        *
+        *   [ 실행 - 객체 -> 문자]
+        *
+        *   • ${number}: 10000
+            • ${{number}}: 10,000
+            *
+            *   컨버전 서비스를 적용한 결과 MyNumberFormatter가 적용되어서 10,000 문자가 출력된 것을 확인할 수 있다.
+            *
+            *
+            [ 실행 - 문자 -> 객체]
+
+            MyNumberFormatter : text=10,000, locale=ko_KR
+            data = 10000
+            *
+            *   "10,000" 이라는 포맷팅 된 문자가 Integer 타입의 숫자 10000으로 정상 변환 된 것을 확인할 수 있다.
         * */
     }
 }
